73d8a18875f91b360f09c317c7cf732b86feebd1
Keijiro Takahashi <keijiro@gmail.com>
2020-11-28 22:06:17 +0900

    initial commit

---
+++ Assets/SimpleCompositor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+public class SimpleCompositor : MonoBehaviour
+{
+    public Texture colorTexture;
+    public Texture matteTexture;
+    public float depth = 1;
+    public float threshold = 0.01f;
+
+    Material _material;
+    Bounds _bounds = new Bounds(Vector3.zero, Vector3.one * 1e+5f);
+
+    void OnDestroy()
+    {
+        if (_material != null)
+        {
+            if (Application.isPlaying)
+                Destroy(_material);
+            else
+                DestroyImmediate(_material);
+            _material = null;
+        }
+    }
+
+    void Update()
+    {
+        if (colorTexture == null || matteTexture == null) return;
+
+        if (_material == null)
+        {
+            var shader = Shader.Find("Hidden/SimpleCompositor");
+            _material = new Material(shader);
+        }
+
+        _material.SetTexture("_ColorTex", colorTexture);
+        _material.SetTexture("_MatteTex", matteTexture);
+        _material.SetFloat("_Depth", depth);
+        _material.SetFloat("_Threshold", threshold);
+
+        Graphics.DrawProcedural
+          (_material, _bounds, MeshTopology.Triangles, 6, 1,
+           null, null, ShadowCastingMode.Off, false, gameObject.layer);
+    }
+}
---
+++ Assets/SimpleCompositor.shader
@@ -0,0 +1,56 @@
+﻿Shader "Hidden/SimpleCompositor"
+{
+    Properties
+    {
+        _ColorTex("", 2D) = "" {}
+        _MatteTex("", 2D) = "" {}
+    }
+
+    CGINCLUDE
+
+#include "UnityCG.cginc"
+
+sampler2D _ColorTex;
+sampler2D _MatteTex;
+float _Depth;
+float _Threshold;
+
+void Vertex(uint vid : SV_VertexID,
+            out float4 vertex : SV_Position,
+            out float2 uv : TEXCOORD0)
+{
+    float x = (vid < 2 || vid == 3) ? 0 : 1;
+    float y = vid & 1;
+
+    float4 temp = mul(UNITY_MATRIX_P, float4(0, 0, -_Depth, 1));
+    float d = temp.z / temp.w;
+
+    vertex = float4(float2(x, y) * 2 - 1, d, 1);
+    uv = float2(x, 1 - y);
+}
+
+float4 Fragment(float4 vertex : SV_Position,
+                float2 uv : TEXCOORD0) : SV_Target
+{
+    float alpha = tex2D(_MatteTex, uv).r;
+    clip(alpha - _Threshold);
+    return float4(tex2D(_ColorTex, uv).rgb, alpha);
+}
+
+    ENDCG
+
+    SubShader
+    {
+        Tags { "Queue" = "Overlay" }
+        Pass
+        {
+            Blend SrcAlpha OneMinusSrcAlpha
+            ZWrite On
+            Cull off
+            CGPROGRAM
+            #pragma vertex Vertex
+            #pragma fragment Fragment
+            ENDCG
+        }
+    }
+}
